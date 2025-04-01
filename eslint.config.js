01d478545c7d3deac1d8eecf0205c06b1f891a34
pezholio <pezholio@gmail.com>
2025-04-01 12:33:29 +0100

    Add eslint

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import globals from "globals";
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+
+export default defineConfig([
+  globalIgnores(["dist/**"]),
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: globals.browser } },
+  { files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["playwright.config.js", "vite.config.js"], languageOptions: { globals: globals.node } },
+  tseslint.configs.recommended,
+]);
